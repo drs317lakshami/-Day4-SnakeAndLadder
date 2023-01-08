28466e7be64399680c374ca8bca78ef38c387e89
drs317lakshami <devrajkumar317@gmail.com>
2023-01-08 13:42:20 +0530

    Added : Ensure Player gets to exact winning position 100

--- src/com/bridgelabz/snakeandladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeLadder.java
@@ -7,48 +7,46 @@ public class SnakeLadder {
 		int start = 0;
 		int userPosition = 0;
 		int count = 0;
-		
-		//print the starting position 
+
 		System.out.println("Player Starting Position will be:~ ");
-		System.out.println(+start);   //just print out to start value
-		
-		/*
-		 *create instance of random class
-		 *Then Invoke methods
-		 *Generate numbers in the range 1to6
-		 * print dice roll result
-		 */
-		Random random = new Random();     //create an random object
+		System.out.println(+start); // just print out to start value
+
+		Random random = new Random();
 		while (userPosition != 100) {
 			count++;
-		
-		int dice = random.nextInt(6)+1;   //Invoke methods and generating numbers
-		System.out.println("dice roll result: "+dice);  //print dice roll result
-	    
-		int cases = random.nextInt(3); //generating number between 0to3
-		
-		switch (cases) { //Using switch keyword
-		case 0:
-			System.out.println("No Play!!");   //print statement to be executed
-			break;    //break out of the switch block
-			
-		case 1:
-			System.out.println("Ladder!");   //print statement to be executed
-			userPosition = userPosition + dice; 
-			break;
-			
-		default:  
-			System.out.println("Snake!");
-			userPosition = userPosition - dice;
 
-			if (userPosition < 0) {
-				userPosition = 0;
+			int dice = random.nextInt(6) + 1;
+			System.out.println("dice roll result: " + dice);
+
+			int cases = random.nextInt(3); // generating number between 0to3
+
+			switch (cases) { // Using switch keyword
+			case 0:
+				System.out.println("No Play!!"); // print statement to be executed
+				break; // break out of the switch block
+
+			case 1:
+				System.out.println("Ladder!"); // print statement to be executed
+				userPosition = userPosition + dice;
+				if (userPosition > 100) // To check condition is grater
+				{
+					userPosition = dice;
+				}
+				break;
+
+			default:
+				System.out.println("Snake!");
+				userPosition = userPosition - dice;
+
+				if (userPosition < 0) {
+					userPosition = 0;
+				}
 			}
+			System.out.println("Presnt Position of the User: " + userPosition); // printing present position
 		}
-		System.out.println("Presnt Position of the User: " + userPosition); 
+		System.out.println("User has rolled " + count + " times to win!"); // printing the user rolled how many time to
+																			// win
+
 	}
-	System.out.println("User has rolled "+count+" times to win!"); 
-	
-	}
-		
+
 }
