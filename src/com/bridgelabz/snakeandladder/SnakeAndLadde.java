a2442081e6251205443890316fb800af89676f19
drs317lakshami <devrajkumar317@gmail.com>
2023-01-08 13:18:11 +0530

    Welcome To Snake And Ladder Simulator

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadde.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadde {
+
+    public static void main(String [] args){
+
+        System.out.println("Welcome To Snake And Ladder Simulator");
+    }
+
+}
